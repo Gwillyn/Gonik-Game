08c11f9bb05fd174121cc128905e821363048597
Gwillyn Donaghy <gwillynd@gmail.com>
2024-04-08 13:54:51 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 // These variables encompass the input where you put your name to begin the game.
 const submit = document.querySelector('#nameTagSubmit');
 submit.addEventListener('click', nameChange);
-const player = document.querySelector('#player');
+let player = document.querySelector('#player');
 const nameText = document.querySelector('#nameTagText');
 const game = document.querySelector('.game'); //This is the whole game window
 const nameInput = document.querySelector('.nameTagBlock');
 const header = document.querySelector('#header');
 
 
+
 // This hides the name input and shows the game input when filled sufficiently. Also, uses the name to refer to the player for the rest of the game.
 
-function nameChange(epic) {
+function nameChange() {
     if (nameText.value <= 1) {
         nameText.placeholder = "You Need a Name";
     }
@@ -52,22 +53,43 @@ const alienStats = document.querySelector('.alienStats');
 // These are the places (locations) that will occur throughout the game
 let places = [
     {
-        name: "deep space",
-        "b-text": ["Space Station", "Light Travel", "Close the Black Hole"],
-        "b-function": [spaceStation, lightTravel, blackHole],
-        text: `You are in deep space.`
+        name: "cockpit",
+        "b-text": ["Deep Space", "Deep Space", "Deep Space"],
+        "b-function": [deepSpace, deepSpace, deepSpace],
+        text: `You are in the cockpit.`
     },
+    //deep space area
+    {
+        name: "deep space",
+        "b-text": ["Cockpit", "Space Station", "Light Travel"],
+        "b-function": [cockpit, spaceStation, lightTravel],
+        text: `You are in deep space. <br><br>You look at the galaxy map. <br><br> <u>What do you want to do?<u>`
+    },
+    //Space station places 
     {
         name: "space station",
-        "b-text": ["Phaser (50 credits)", "Rockets (150 credits)", "Deep Space"],
-        "b-function": [buyPhaser, buyRockets, deepSpace],
-        text: `You arrive at the space station. <br><br>Many stores are open, but you are only interested in munitions. <br><br><u>What will you buy?</u>`
+        "b-text": ["Weapon Store", "Mechanic", "Deep Space"],
+        "b-function": [weaponStore, mechanic, deepSpace],
+        text: `You arrive at the space station. <br><br>Many stores are open, but you are only interested in munitions and ship repairs. <br><br><u>What will you buy?</u>`
     },
+    {
+        name: "weapon store",
+        "b-text": ["Phaser (50 credits)", "Rockets (150 credits)", "Space Station"],
+        "b-function": [buyPhaser, buyRockets, spaceStation],
+        text: `You arrive at the Weapon Store. <br><br>The clerk is quiet, but seems to know what he's doing. <br><br><u>What will you buy?</u>`
+    },
+    {
+        name: "Mechanic",
+        "b-text": ["Repair Ship (100 credits)", "", "Space Station"],
+        "b-function": [buyHealth, buyHealth, spaceStation],
+        text: `You arrive at the Weapon Store. <br><br>A loud, dirty guy greets you<br>with a yell <br><br><u>"You want some repairs?"</u>`
+    },
+    //Light travel places
     {
         name: "light travel",
         "b-text": ["Xathor", "Gokr Prime", "Jenki X"],
         "b-function": [xathor, gokr, jenki],
-        text: `You look at the galaxy map. <br><br>There are so many planets to go to, but you're looking for a challenge. <br><br><u>Where will you go?</u>`
+        text: `There are so many planets to go to, but you're looking for a challenge. <br><br><u>Where will you go?</u>`
     }
 ];
 //button start
@@ -86,38 +108,69 @@ function update(place) {
     button3.onclick = place["b-function"][2];
     text.innerHTML = place.text;
     button4.classList.add('hide');
+    button5.classList.add('hide');
+    button6.classList.add('hide');
+    button2.classList.remove('hide')
+}
+
+//Cockpit
+function cockpit() {
+    update(places[0]);
+    imageDesc.style.backgroundImage = "url(https://i.pinimg.com/originals/bc/b4/3f/bcb43f3268d5c86d07234f320ad3fdd4.jpg)"
 }
 
 //Deep Space
 function deepSpace() {
-    update(places[0]);
+    update(places[1]);
+    button4.classList.remove('hide');
+    button4.innerText = "Close Black Hole";
+    button4.onclick = blackHole;
     imageDesc.style.backgroundImage = "url(https://wallpaperaccess.com/full/634686.jpg)";
     imageDesc.style.backgroundPositionY = "-40px";
 }
+
 //Space Station
 function spaceStation() {
-    update(places[1]);
+    update(places[2]);
     imageDesc.style.backgroundImage = "url(https://i.pinimg.com/originals/11/e9/6e/11e96e0d427b6eb2304c2753f85744ca.jpg)";
 }
+function weaponStore() {
+    update(places[3]);
+}
+function buyRockets() { }
+function buyPhaser() {
+    alert()
+}
+function mechanic() {
+    update(places[4]);
+    button2.classList.add('hide');
+ }
+ function buyHealth() { }
+
 //Planetary Exploration
 function lightTravel() {
-    update(places[2]);
+    update(places[5]);
     button4.classList.remove('hide');
     button4.innerText = "Deep Space"
     button4.onclick = deepSpace;
     imageDesc.style.backgroundImage = "url(https://th.bing.com/th/id/OIP.Fv3z5jsHU6GghlKc5nSHiwHaEo?rs=1&pid=ImgDetMain)";
     imageDesc.style.backgroundPositionY = "-30px";
 }
-//Close the Black Hole
-function blackHole() {
-    update(places[1]);
-}
-//working on these
-function buyPhaser() {
-    alert()
-}
-function buyRockets() { }
 function gokr() { }
 function xathor() { }
 function jenki() { }
+
+//Close the Black Hole
+function blackHole() {}
+
+
+//working on these
+
+
+
+
+
+
+
+
 
